59ea4806c357dfcfe85a6c29ce77b6edd94f81d2
Giwan <giwan.kim@gmail.com>
2020-10-21 15:06:29 +0900

    Add src/data/data.py

---
+++ src/data/data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+
+
+class NowcastingDataset(torch.utils.data.Dataset):
+    def __init__(self, paths, test=False):
+        self.paths = paths
+        self.test = test
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        data = np.load(path)
+        x = data[:, :, :4]
+        x = x / 255.0
+        x = x.astype(np.float32)
+        x = torch.tensor(x, dtype=torch.float)
+        x = x.permute(2, 0, 1)
+        if self.test:
+            return x
+        else:
+            y = data[:, :, 4]
+            y = y / 255.0
+            y = y.astype(np.float32)
+            y = torch.tensor(y, dtype=torch.float)
+            y = y.unsqueeze(-1)
+            y = y.permute(2, 0, 1)
+
+            return x, y
